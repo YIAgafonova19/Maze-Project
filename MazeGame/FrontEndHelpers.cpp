2add584a6a09328428b91ac38ee734e0b59d38e3
Martin Martinov <mvmartinov19@codingburgas.bg>
2021-11-07 23:20:00 +0200

    Add Comments for FrontEnd.h

--- MazeGame/FrontEndHelpers.cpp
+++ MazeGame/FrontEndHelpers.cpp
@@ -4,13 +4,14 @@
 #include "FrontEndHelpers.h"
 
 using namespace std;
-
+// This function is for Console Color
 void setColor(int color) {
     if (STD_OUTPUT_HANDLE) {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
     }
 }
 
+// This function is for Console Position
 void gotoxy(int x, int y) {
     COORD c;
     c.X = x;
@@ -18,6 +19,7 @@ void gotoxy(int x, int y) {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
 
+// This function is for Console Cursor
 void setCursor(bool visible) {
     CONSOLE_CURSOR_INFO info;
     info.dwSize = 100;
@@ -25,6 +27,7 @@ void setCursor(bool visible) {
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
 }
 
+// This function is for Full Screen
 void setFullScreen() {
     SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE), CONSOLE_FULLSCREEN_MODE, 0);
 }
