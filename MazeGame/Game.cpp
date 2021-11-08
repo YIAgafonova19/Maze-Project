a7197b46078951fa69604ad8d85039d109380a1b
DSDimitrov19 <DSDimitrov19@codingburgas.bg>
2021-11-08 09:40:14 +0200

    Fix Game Win screen issue.

--- MazeGame/Game.cpp
+++ MazeGame/Game.cpp
@@ -11,6 +11,7 @@
 using namespace std;
 
 bool inGame;
+bool isWin;
 char gameKeyPressed;
 int mazeSizes[] = { 10, 15, 20 };
 int currentDifficulty;
@@ -35,26 +36,30 @@ void processPlayerInput() {
 			break;
 		case(KEY_ESC):
 			inGame = false;
+			isWin = false;
 			break;
 		default:
 			break;
     }
 }
 
-void drawGameEnd() {
+void drawGameEnd(bool isWin) {
 	system("CLS");
-	setColor(11);
-	cout << "          _______                           _________ _       \n";
-	cout << "|\\     /|(  ___  )|\\     /|        |\\     /|\\__   __/( (    /|\n";
-	cout << "( \\   / )| (   ) || )   ( |        | )   ( |   ) (   |  \\  ( |\n";
-	cout << " \\ (_) / | |   | || |   | |        | | _ | |   | |   |   \\ | |\n";
-	cout << "  \\   /  | |   | || |   | |        | |( )| |   | |   | (\\ \\) |\n";
-	cout << "   ) (   | |   | || |   | |        | || || |   | |   | | \\   |\n";
-	cout << "   | |   | (___) || (___) |        | () () |___) (___| )  \\  |\n";
-	cout << "   \\_/   (_______)(_______)        (_______)\\_______/|/    )_)\n";
-	cout << "\n\n";
-	system("PAUSE");
-	system("CLS");
+	if (isWin) {
+		setColor(11);
+		cout << "          _______                           _________ _       \n";
+		cout << "|\\     /|(  ___  )|\\     /|        |\\     /|\\__   __/( (    /|\n";
+		cout << "( \\   / )| (   ) || )   ( |        | )   ( |   ) (   |  \\  ( |\n";
+		cout << " \\ (_) / | |   | || |   | |        | | _ | |   | |   |   \\ | |\n";
+		cout << "  \\   /  | |   | || |   | |        | |( )| |   | |   | (\\ \\) |\n";
+		cout << "   ) (   | |   | || |   | |        | || || |   | |   | | \\   |\n";
+		cout << "   | |   | (___) || (___) |        | () () |___) (___| )  \\  |\n";
+		cout << "   \\_/   (_______)(_______)        (_______)\\_______/|/    )_)\n";
+		cout << "\n\n";
+
+		system("PAUSE");
+		system("CLS");
+	}
 }
 
 void playGame(int difficulty) {
@@ -85,6 +90,7 @@ void playGame(int difficulty) {
 		if (checkFinalCondition()) {
 			setFinalScore();
 			inGame = false;
+			isWin = true;
 		}
 	}
 
@@ -92,5 +98,5 @@ void playGame(int difficulty) {
 	destroyPlayer();
 	destroyScore();
 
-	drawGameEnd();
+	drawGameEnd(isWin);
 }
